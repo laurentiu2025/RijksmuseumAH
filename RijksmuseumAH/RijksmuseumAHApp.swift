00a2705f49c6b9d029bdc72518954684d98b8789
laurentiu2025 <laurentiu.software2025@gmail.com>
2025-09-17 10:16:53 +0200

    chore: add initial project

---
+++ RijksmuseumAH/RijksmuseumAHApp.swift
@@ -0,0 +1,17 @@
+//
+//  RijksmuseumAHApp.swift
+//  RijksmuseumAH
+//
+//  Created by Laurentiu Cociu on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RijksmuseumAHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
